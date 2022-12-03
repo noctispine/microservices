96c8a7f276de268a9bb361e855ddaf4d554b3f85
Eren Çam <eren@Erens-Air.home>
2022-12-04 00:23:44 +0300

    feat: wrapper for response json errors

---
+++ services/user-service/pkg/responses/errors.go
@@ -0,0 +1,20 @@
+package responses
+
+import (
+	"fmt"
+
+	"github.com/capstone-project-bunker/backend/internal/translations"
+	"github.com/gin-gonic/gin"
+)
+
+func AbortWithStatusJSONError(c *gin.Context, code int, err error) {
+	c.AbortWithStatusJSON(code, gin.H{
+		"error": err.Error()})
+}
+
+func AbortWithStatusJSONValidationErrors(c *gin.Context, code int, err error) {
+	errs := translations.TranslateError(err, translations.EnTrans)
+	fmt.Println(errs)
+    c.AbortWithStatusJSON(code , gin.H{
+		"errors": translations.StringfyJSONErrArr(errs)})
+}
