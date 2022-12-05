a35c985115a674fef6329323369f51e3116859f1
Eren Çam <eren@Erens-Air.home>
2022-12-06 01:30:54 +0300

    feat: add redis cache

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -5,15 +5,18 @@ import (
 	"log"
 	"os"
 
+	"github.com/capstone-project-bunker/backend/cmd/cache"
 	dbPackage "github.com/capstone-project-bunker/backend/cmd/db"
 	userQ "github.com/capstone-project-bunker/backend/cmd/db/queries/user"
 	"github.com/capstone-project-bunker/backend/internal/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 var userHandler *handlers.UserHandler
+var rdb *redis.Client
 
 func init() {
 	err := godotenv.Load(".env")
@@ -23,7 +26,9 @@ func init() {
 
 	db = dbPackage.GetDatabase()
 	queriesDB := userQ.New(db)
-	userHandler = handlers.NewUserHandler(queriesDB)
+	rdb = cache.NewRedisClient()
+	
+	userHandler = handlers.NewUserHandler(queriesDB, rdb)
 }
 
 func main() {
@@ -32,6 +37,7 @@ func main() {
 	r.GET("/", userHandler.GetAll)
 	r.GET("/by-email", userHandler.GetByEmail)
 	r.DELETE("/by-email", userHandler.DeleteByEmail)
+	r.DELETE("/:id", userHandler.DeleteById)
 
 	log.Fatal(r.Run(":" + os.Getenv("DEV_PORT")))
 }
