40e6abca357098d71780e30dab26ddd8c2f165f1
Eren Çam <eren@Erens-Air.home>
2022-12-07 21:40:22 +0300

    fix: module name

---
+++ services/users/config/db/query.sql
@@ -0,0 +1,27 @@
+-- name: GetAllByNames :many
+SELECT * FROM users
+ORDER BY name;
+
+-- name: GetAll :many
+SELECT * FROM users;
+
+-- name: GetByEmail :one
+SELECT * FROM users
+WHERE email = $1;
+
+-- name: GetById :one
+SELECT * FROM users
+WHERE id = $1;
+
+-- name: Create :exec
+INSERT INTO users (
+    name, surname, email
+) VALUES (
+    $1, $2, $3
+);
+
+-- name: DeleteByEmail :execrows
+DELETE FROM users WHERE email = $1;
+
+-- name: DeleteById :execrows
+DELETE FROM users WHERE id = $1;
