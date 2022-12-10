472884c60a3b99e3b467dfae3d103d2964d3bd8a
Eren Çam <eren@Erens-Air.home>
2022-12-11 02:42:25 +0300

    docs: configure docker networks & change db and redis's hosts according to networks

--- services/users/cmd/cache/cache.go
+++ services/users/cmd/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,13 +10,14 @@ import (
 
 func NewRedisClient() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:" + os.Getenv("REDIS_PORT"),
+		
+		Addr:     fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
 	if rdb.Ping().Val() == "" {
-		log.Fatal("CACHE ERROR")
+		log.Fatal("CACHE ERROR: ", rdb.Ping())
 	}
 
 	return rdb
