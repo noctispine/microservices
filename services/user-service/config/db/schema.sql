28d2d994e74dd29f0c298a7d0b1dea8ce1546d0c
Eren Çam <eren@Erens-Air.home>
2023-04-17 23:59:06 +0300

    chore: moved to users

--- services/user-service/config/db/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    name varchar(50) NOT NULL,
-    surname varchar(50) NOT NULL,
-    email varchar(72) NOT NULL UNIQUE
-);
