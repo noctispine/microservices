8e647b71936258878b5fd9982dbfc4943842f1ac
Eren Çam <eren@Erens-Air.home>
2023-04-28 03:43:44 +0300

    feat: add key constants

--- services/shared/cconstants/keys.go
+++ services/shared/cconstants/keys.go
@@ -1,8 +1,15 @@
 package cconstants
 
+// shared context keys
+type keys struct {
+	USER_ID string
+	USER_ROLE string
+}
 
-
-
+var KEYS = keys{
+	USER_ID: "userId",
+	USER_ROLE: "userRole",
+}
 
 const (
 	UserID = "userId"
@@ -14,7 +21,6 @@ type env struct {
 	DEVELOPMENT string
 	PRODUCTION string
 }
-
 
 var ENV = env{
 	APP_ENV: "APP_ENV",
